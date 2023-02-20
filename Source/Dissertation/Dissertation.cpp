8e5afe43b72807dde18a5df170600e9447c6d4b4
NiicoNiicoDev <niicodev@gmail.com>
2023-02-20 18:25:31 +0000

    Initial commit

---
+++ Source/Dissertation/Dissertation.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Dissertation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dissertation, "Dissertation" );
